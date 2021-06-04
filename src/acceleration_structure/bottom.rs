be8662cd4278aeb3e6de4441d8e8dd4d37a59e69
evopen <520dhh@gmail.com>
2021-06-04 18:08:05 +0800

    add bottom level acc. structure definition

---
+++ src/acceleration_structure/bottom.rs
@@ -0,0 +1,126 @@
+use std::ffi::CString;
+use std::sync::Arc;
+
+use ash::vk;
+use ash::vk::Handle;
+
+use crate::Device;
+
+pub struct BottomAceelerationStructureRef {
+    handle: vk::AccelerationStructureKHR,
+    device_address: u64,
+    device: Device,
+}
+
+#[derive(Clone)]
+pub struct BottomAccelerationStructure {
+    pub(crate) inner: Arc<BottomAceelerationStructureRef>,
+}
+
+impl BottomAccelerationStructure {
+    pub(crate) fn new(
+        name: Option<&str>,
+        device: &Device,
+        geometries: &[&super::TriangleGeometry],
+    ) {
+        let vk_geometries = geometries
+            .iter()
+            .map(|t| t.acceleration_structure_geometry)
+            .collect::<Vec<_>>();
+        let triangle_count = geometries
+            .iter()
+            .map(|t| t.triangle_count)
+            .collect::<Vec<_>>();
+        unsafe {
+            let size_info = device
+                .inner
+                .acceleration_structure_loader
+                .get_acceleration_structure_build_sizes(
+                    vk::AccelerationStructureBuildTypeKHR::DEVICE,
+                    &vk::AccelerationStructureBuildGeometryInfoKHR::builder()
+                        .flags(vk::BuildAccelerationStructureFlagsKHR::PREFER_FAST_TRACE)
+                        .ty(vk::AccelerationStructureTypeKHR::BOTTOM_LEVEL)
+                        .geometries(&vk_geometries)
+                        .build(),
+                    &triangle_count,
+                );
+            let as_buffer = device.create_buffer(
+                Some(&format!(
+                    "{} buffer",
+                    name.unwrap_or("acceleration structure")
+                )),
+                size_info.acceleration_structure_size,
+                vk::BufferUsageFlags::ACCELERATION_STRUCTURE_STORAGE_KHR
+                    | vk::BufferUsageFlags::SHADER_DEVICE_ADDRESS,
+                gpu_allocator::MemoryLocation::GpuOnly,
+            );
+            let handle = device
+                .inner
+                .acceleration_structure_loader
+                .create_acceleration_structure(
+                    &vk::AccelerationStructureCreateInfoKHR::builder()
+                        .ty(vk::AccelerationStructureTypeKHR::BOTTOM_LEVEL)
+                        .buffer(as_buffer.inner.handle)
+                        .size(size_info.acceleration_structure_size)
+                        .build(),
+                    None,
+                )
+                .unwrap();
+
+            if let Some(name) = name {
+                device
+                    .inner
+                    .pdevice
+                    .instance
+                    .inner
+                    .debug_utils_loader
+                    .as_ref()
+                    .unwrap()
+                    .debug_utils_set_object_name(
+                        device.handle().handle(),
+                        &vk::DebugUtilsObjectNameInfoEXT::builder()
+                            .object_handle(handle.as_raw())
+                            .object_type(vk::ObjectType::ACCELERATION_STRUCTURE_KHR)
+                            .object_name(CString::new(name).unwrap().as_ref())
+                            .build(),
+                    )
+                    .unwrap();
+            }
+            let scratch_buffer = device.create_buffer(
+                Some(&format!(
+                    "{} scratch buffer",
+                    name.unwrap_or("acceleration structure")
+                )),
+                size_info.build_scratch_size,
+                vk::BufferUsageFlags::ACCELERATION_STRUCTURE_STORAGE_KHR
+                    | vk::BufferUsageFlags::SHADER_DEVICE_ADDRESS,
+                crate::MemoryLocation::GpuOnly,
+            );
+
+            let build_geometry_info = vk::AccelerationStructureBuildGeometryInfoKHR::builder()
+                .flags(vk::BuildAccelerationStructureFlagsKHR::PREFER_FAST_TRACE)
+                .ty(vk::AccelerationStructureTypeKHR::BOTTOM_LEVEL)
+                .geometries(&vk_geometries)
+                .dst_acceleration_structure(handle)
+                .mode(vk::BuildAccelerationStructureModeKHR::BUILD)
+                .scratch_data(vk::DeviceOrHostAddressKHR {
+                    device_address: scratch_buffer.device_address(),
+                })
+                .build();
+
+            let build_range_infos = geometries
+                .iter()
+                .map(|t| t.build_range_info)
+                .collect::<Vec<_>>();
+
+            let mut cmd_buf = device.create_command_buffer(
+                Some("build acceleration structure"),
+                device.compute_queue_family_index(),
+            );
+            cmd_buf.encode(|recorder| {
+                recorder.build_acceleration_structure_raw(build_geometry_info, &build_range_infos);
+            });
+            device.compute_queue().submit_blocking(&[cmd_buf]);
+        }
+    }
+}
