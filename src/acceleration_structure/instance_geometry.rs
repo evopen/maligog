f671a3bc87e846138091b61b035c5e1874b2ecd3
evopen <520dhh@gmail.com>
2021-06-05 15:33:34 +0800

    dont use host address

--- src/acceleration_structure/instance_geometry.rs
+++ src/acceleration_structure/instance_geometry.rs
@@ -2,36 +2,31 @@ use std::ffi::c_void;
 
 use ash::vk;
 
+use crate::Device;
+
 unsafe impl Sync for InstanceGeometry {}
 unsafe impl Send for InstanceGeometry {}
 
+#[derive(Clone)]
 pub struct InstanceGeometry {
     pub(crate) acceleration_structure_geometry: vk::AccelerationStructureGeometryKHR,
     pub(crate) build_range_info: vk::AccelerationStructureBuildRangeInfoKHR,
-    pub(crate) instance_count: u32,
+    blas_instances: Vec<super::BLASInstance>,
+    array_of_pointer_buffer: crate::Buffer,
 }
 
 impl InstanceGeometry {
-    pub fn new(
-        bottoms: &[&super::BottomAccelerationStructure],
-        transforms: &[vk::TransformMatrixKHR],
-    ) -> Self {
-        let instances = bottoms
+    pub fn new(device: &Device, blas_instances: &[super::BLASInstance]) -> Self {
+        let instance_buffer_addresses: Vec<u64> = blas_instances
             .iter()
-            .zip(transforms)
-            .map(|(bottom, transform)| {
-                vk::AccelerationStructureInstanceKHR {
-                    transform: transform.clone(),
-                    instance_custom_index_and_mask: 0 | (0xFF << 24),
-                    instance_shader_binding_table_record_offset_and_flags: 0
-                        | (vk::GeometryInstanceFlagsKHR::TRIANGLE_FACING_CULL_DISABLE.as_raw()
-                            << 24),
-                    acceleration_structure_reference: vk::AccelerationStructureReferenceKHR {
-                        device_handle: bottom.inner.device_address,
-                    },
-                }
-            })
-            .collect::<Vec<_>>();
+            .map(|i| i.instance_buffer.as_ref().unwrap().device_address())
+            .collect();
+        let array_of_pointer_buffer = device.create_buffer_init(
+            Some("array of pointers"),
+            bytemuck::cast_slice(&instance_buffer_addresses),
+            vk::BufferUsageFlags::ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_KHR,
+            gpu_allocator::MemoryLocation::GpuOnly,
+        );
 
         let geometry = vk::AccelerationStructureGeometryKHR::builder()
             .geometry_type(vk::GeometryTypeKHR::INSTANCES)
@@ -41,23 +36,27 @@ impl InstanceGeometry {
             )
             .geometry(vk::AccelerationStructureGeometryDataKHR {
                 instances: vk::AccelerationStructureGeometryInstancesDataKHR::builder()
-                    .array_of_pointers(false)
+                    .array_of_pointers(true)
                     .data(vk::DeviceOrHostAddressConstKHR {
-                        host_address: instances.as_ptr() as *const c_void,
+                        device_address: array_of_pointer_buffer.device_address(),
                     })
                     .build(),
             })
             .build();
 
-        let instance_count = instances.len() as u32;
         let build_range_info = vk::AccelerationStructureBuildRangeInfoKHR::builder()
-            .primitive_count(instance_count)
+            .primitive_count(blas_instances.len() as u32)
             .primitive_offset(0) // offset to index buffer in bytes
             .build();
         Self {
             acceleration_structure_geometry: geometry,
-            instance_count,
             build_range_info,
+            blas_instances: blas_instances.to_owned(),
+            array_of_pointer_buffer,
         }
+    }
+
+    pub fn instance_count(&self) -> u32 {
+        self.blas_instances.len() as u32
     }
 }
