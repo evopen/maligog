d6099d3d13fcfebbf0393c7e827ee4273a96cf0b
evopen <520dhh@gmail.com>
2021-07-21 22:40:07 +0800

    procedural hit group

--- src/ray_tracing/hit_group.rs
+++ src/ray_tracing/hit_group.rs
@@ -21,8 +21,27 @@ impl TrianglesHitGroup {
 #[derive(Clone)]
 pub struct ProceduralHitGroup {
     closest_hit_shader: ShaderStage,
-    intersection_hit_shader: ShaderStage,
+    intersection_shader: ShaderStage,
     any_hit_shader: Option<ShaderStage>,
+}
+
+impl ProceduralHitGroup {
+    pub fn new(
+        closest_hit_shader: &ShaderStage,
+        intersection_shader: &ShaderStage,
+        any_hit_shader: Option<&ShaderStage>,
+    ) -> Self {
+        assert!(closest_hit_shader.stage == vk::ShaderStageFlags::CLOSEST_HIT_KHR);
+        assert!(intersection_shader.stage == vk::ShaderStageFlags::INTERSECTION_KHR);
+        if let Some(any_hit_shader) = any_hit_shader {
+            assert!(any_hit_shader.stage == vk::ShaderStageFlags::ANY_HIT_KHR);
+        }
+        Self {
+            closest_hit_shader: closest_hit_shader.clone(),
+            intersection_shader: intersection_shader.clone(),
+            any_hit_shader: any_hit_shader.map(|a| a.to_owned()),
+        }
+    }
 }
 
 pub trait HitGroup: dyn_clone::DynClone + 'static {
@@ -63,7 +82,7 @@ impl HitGroup for ProceduralHitGroup {
     fn shader_stage_create_infos(&self) -> Vec<vk::PipelineShaderStageCreateInfo> {
         let mut infos = Vec::new();
         infos.push(self.closest_hit_shader.shader_stage_create_info());
-        infos.push(self.intersection_hit_shader.shader_stage_create_info());
+        infos.push(self.intersection_shader.shader_stage_create_info());
         if let Some(any_hit_shader) = self.any_hit_shader.as_ref() {
             infos.push(any_hit_shader.shader_stage_create_info());
         }
